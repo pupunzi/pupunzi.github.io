dbd9d3eae49f6ca63a454baf4935a7e88c702067
pupunzi <matteo@open-lab.com>
2012-10-18 23:34:32 +0200

    updated to v. 1.6

--- jquery.mb.CSSAnimate/inc/jquery.mb.CSSAnimate.js
+++ jquery.mb.CSSAnimate/inc/jquery.mb.CSSAnimate.js
@@ -1,6 +1,6 @@
 /*******************************************************************************
  jquery.mb.components
- Copyright (c) 2001-2011. Matteo Bicocchi (Pupunzi); Open lab srl, Firenze - Italy
+ Copyright (c) 2001-2012. Matteo Bicocchi (Pupunzi); Open lab srl, Firenze - Italy
  email: mbicocchi@open-lab.com
  site: http://pupunzi.com
  blog: http://pupunzi.open-lab.com
@@ -14,8 +14,8 @@
 /*
  *
  * jQuery.mb.components: jquery.mb.CSSAnimate
- * version: 1.0- 04/12/11 - 18
- * © 2001 - 2011 Matteo Bicocchi (pupunzi), Open Lab
+ * version: 1.6
+ * © 2001 - 2012 Matteo Bicocchi (pupunzi), Open Lab
  *
  * Licences: MIT, GPL
  * http://www.opensource.org/licenses/mit-license.php
@@ -29,7 +29,6 @@
  @opt        -> the CSS object (ex: {top:300, left:400, ...})
  @duration   -> an int for the animation duration in milliseconds
  @ease       -> ease  ||  linear || ease-in || ease-out || ease-in-out  ||  cubic-bezier(<number>, <number>,  <number>,  <number>)
- @properties -> properties to wich CSS3 transition should be applied.
  @callback   -> a callback function called once the transition end
 
  example:
@@ -37,97 +36,143 @@
  $(this).CSSAnimate({top: t, left:l, width:w, height:h}, 2000, "ease-out", "all", function() {el.anim();})
  */
 
+
+
 /*Browser detection patch*/
-$.browser.mozilla = /firefox/.test(navigator.userAgent.toLowerCase());
+$.browser.mozilla = /mozilla/.test(navigator.userAgent.toLowerCase());
 $.browser.webkit = /webkit/.test(navigator.userAgent.toLowerCase());
 $.browser.opera = /opera/.test(navigator.userAgent.toLowerCase());
 $.browser.msie = /msie/.test(navigator.userAgent.toLowerCase());
 
+$.fn.CSSAnimate = function(opt, duration, delay, ease, callback) {
+	return this.each(function() {
 
-$.fn.CSSAnimate = function(opt, duration, ease, properties, callback) {
-  return this.each(function() {
+		var el = $(this);
 
-    var el = $(this);
+		if (el.length === 0 || !opt) {return;}
 
-    if (el.length === 0 || !opt) {return;}
+		if (typeof duration == "function") {callback = duration; duration = $.fx.speeds["_default"];}
+		if (typeof delay == "function") {callback = delay; delay=0}
+		if (typeof ease == "function") {callback = ease; ease = "cubic-bezier(0.65,0.03,0.36,0.72)";}
 
-    if (typeof duration == "function") {callback = duration;}
-    if (typeof ease == "function") {callback = ease;}
-    if (typeof properties == "function") {callback = properties;}
 
-    if(typeof duration == "string"){
-      for(var d in $.fx.speeds){
-        if(duration==d){
-          duration= $.fx.speeds[d];
-          break;
-        }else{
-          duration=null;
-        }
-      }
-    }
+		if(typeof duration == "string"){
+			for(var d in $.fx.speeds){
+				if(duration==d){
+					duration= $.fx.speeds[d];
+					break;
+				}else{
+					duration=null;
+				}
+			}
+		}
 
-    if (!duration) {duration = $.fx.speeds["_default"];}
+		//http://cssglue.com/cubic
+		//  ease  |  linear | ease-in | ease-out | ease-in-out  |  cubic-bezier(<number>, <number>,  <number>,  <number>)
 
-    if (!ease) {ease = "cubic-bezier(0.65,0.03,0.36,0.72)";}
-    if (!properties) {properties = "all";}
+		if (!jQuery.support.transition) {
 
-    //http://cssglue.com/cubic
-    //  ease  |  linear | ease-in | ease-out | ease-in-out  |  cubic-bezier(<number>, <number>,  <number>,  <number>)
+			for(var o in opt){
+				if (o==="transform"){
+					delete opt[o];
+				}
+				if (o==="transform-origin"){
+					delete opt[o];
+				}
+				if (opt[o]==="auto"){
+					delete opt[o];
+				}
+			}
 
-    if (!jQuery.support.transition) {
-      el.animate(opt, duration, callback);
-      return;
-    }
+			if(!callback || typeof callback==="string")
+				callback ="linear";
 
-    var sfx = "";
-    var transitionEnd = "TransitionEnd";
-    if ($.browser.webkit) {
-      sfx = "-webkit-";
-      transitionEnd = "webkitTransitionEnd";
-    } else if ($.browser.mozilla) {
-      sfx = "-moz-";
-      transitionEnd = "transitionend";
-    } else if ($.browser.opera) {
-      sfx = "-o-";
-      transitionEnd = "oTransitionEnd";
-    } else if ($.browser.msie) {
-      sfx = "-ms-";
-      transitionEnd = "msTransitionEnd";
-    }
+			el.animate(opt, duration, callback);
+			return;
+		}
 
-    for(var o in opt){
-      if (o==="transform"){
-        opt[sfx+"transform"]=opt[o];
-        delete opt[o];
-      }
-      if (o==="transform-origin"){
-        opt[sfx+"transform-origin"]=opt[o];
-        delete opt[o];
-      }
-    }
+		var sfx = "";
+		var transitionEnd = "transitionEnd";
+		if ($.browser.webkit) {
+			sfx = "-webkit-";
+			transitionEnd = "webkitTransitionEnd";
+		} else if ($.browser.mozilla) {
+			sfx = "-moz-";
+			transitionEnd = "transitionend";
+		} else if ($.browser.opera) {
+			sfx = "-o-";
+			transitionEnd = "oTransitionEnd";
+		} else if ($.browser.msie) {
+			sfx = "-ms-";
+			transitionEnd = "msTransitionEnd";
+		}
 
-    el.css(sfx + "transition-property", properties);
-    el.css(sfx + "transition-duration", duration + "ms");
-    el.css(sfx + "transition-timing-function", ease);
+		var prop = [];
+		for(var o in opt){
+			var key = o;
+			if (key==="transform"){
+				key = sfx+"transform";
+				opt[key]=opt[o];
+				delete opt[o];
+			}
+			if (key==="transform-origin"){
+				key = sfx+"transform-origin";
+				opt[key]=opt[o];
+				delete opt[o];
+			}
+			prop.push(key);
 
-    setTimeout(function() {
-      el.css(opt);
-    }, 0);
+			if(!el.css(key))
+				el.css(key,0);
+		}
+		var properties = prop.join(",");
 
-    var endTransition = function() {
-      el.get(0).removeEventListener(transitionEnd, endTransition, false);
-      el.css(sfx + "transition", "");
-      if (typeof callback == "function") callback();
-    };
-    el.get(0).addEventListener(transitionEnd, endTransition, false);
+		el.css(sfx + "transition-property", properties);
+		el.css(sfx + "transition-duration", duration + "ms");
+		el.css(sfx + "transition-delay", delay + "ms");
+		el.css(sfx + "transition-timing-function", ease);
+		el.css(sfx + "backface-visibility","hidden");
 
-  })
+		setTimeout(function() {
+			el.css(opt);
+		}, 0);
+
+		var endTransition = function(e) {
+			$(this).off(transitionEnd);
+			$(this).css(sfx + "transition", "");
+			e.stopPropagation();
+			if (typeof callback == "function") callback();
+			return false;
+		};
+		$(this).on(transitionEnd, endTransition);
+	})
 };
+
+$.fn.CSSAnimateStop=function(){
+	var sfx = "";
+	var transitionEnd = "transitionEnd";
+	if ($.browser.webkit) {
+		sfx = "-webkit-";
+		transitionEnd = "webkitTransitionEnd";
+	} else if ($.browser.mozilla) {
+		sfx = "-moz-";
+		transitionEnd = "transitionend";
+	} else if ($.browser.opera) {
+		sfx = "-o-";
+		transitionEnd = "oTransitionEnd";
+	} else if ($.browser.msie) {
+		sfx = "-ms-";
+		transitionEnd = "msTransitionEnd";
+	}
+
+	$(this).css(sfx + "transition", "");
+	$(this).off(transitionEnd);
+}
 
 // jQuery.support.transition
 // to verify that CSS3 transition is supported (or any of its browser-specific implementations)
 $.support.transition = (function() {
-  var thisBody = document.body || document.documentElement;
-  var thisStyle = thisBody.style;
-  return thisStyle.transition !== undefined || thisStyle.WebkitTransition !== undefined || thisStyle.MozTransition !== undefined || thisStyle.MsTransition !== undefined || thisStyle.OTransition !== undefined;
+	var thisBody = document.body || document.documentElement;
+	var thisStyle = thisBody.style;
+	return thisStyle.transition !== undefined || thisStyle.WebkitTransition !== undefined || thisStyle.MozTransition !== undefined || thisStyle.MsTransition !== undefined || thisStyle.OTransition !== undefined;
 })();
