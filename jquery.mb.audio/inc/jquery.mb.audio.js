2c0ccb103e2a44d112879563fe5f472dd8eb94e6
pupunzi <matteo@open-lab.com>
2013-02-18 23:14:18 +0100

    ssssssasa

--- jquery.mb.audio/inc/jquery.mb.audio.js
+++ jquery.mb.audio/inc/jquery.mb.audio.js
@@ -14,7 +14,7 @@
  *  http://www.opensource.org/licenses/mit-license.php
  *  http://www.gnu.org/licenses/gpl.html
  *
- *  last modified: 18/02/13 22.48
+ *  last modified: 18/02/13 23.14
  *  *****************************************************************************
  */
 /*
@@ -547,12 +547,10 @@ function supportType(audioType) {
 
 				var soundEl = typeof soundID == "string" ? $.mbAudio.sounds[soundID] : soundID;
 
-/*
 				if(!soundEl.started){
 					$.mbAudio.play(soundID);
 					soundEl.started=true;
 				}
-*/
 
 				sprite = typeof sprite == "string" ? soundEl.sprite[sprite] : sprite;
 
